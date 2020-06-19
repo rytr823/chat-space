a28b8a7db48efddc64e5e6daf5200baa75dfbc58
rytr823 <rytr823@gmail.com>
2020-06-19 11:33:02 +0900

    Implementation of automatic update function

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,8 +1,8 @@
-$(function() {
-  function buildHTML(message){
-    if ( message.image ) {
+$(function () {
+  function buildHTML(message) {
+    if (message.image) {
       let html =
-        `<div class="wrapper__chat-main__message-list__messages">
+        `<div class="wrapper__chat-main__message-list__messages" data-message-id=${message.id}>
           <div class="wrapper__chat-main__message-list__messages__post-info">
             <div class="wrapper__chat-main__message-list__messages__post-info__message-info">
               <div class="wrapper__chat-main__message-list__messagespost-info__message-info__name">
@@ -23,7 +23,7 @@ $(function() {
       return html;
     } else {
       let html =
-      `<div class="wrapper__chat-main__message-list__messages">
+        `<div class="wrapper__chat-main__message-list__messages" data-message-id=${message.id}>
       <div class="wrapper__chat-main__message-list__messages__post-info">
         <div class="wrapper__chat-main__message-list__messages__post-info__message-info">
           <div class="wrapper__chat-main__message-list__messagespost-info__message-info__name">
@@ -40,31 +40,31 @@ $(function() {
         </div>
       </div>
     </div>`
-    return html;
+      return html;
     };
   }
 
-  $('.wrapper__chat-main__message-form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function () {
+    let last_message_id = $('.wrapper__chat-main__message-list__messages__post-info:last').data('message-id');
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: 'api/messages',
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: { id: last_message_id }
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.wrapper__chat-main__message-list').append(html);
-      $('.wrapper__chat-main__message-form')[0].reset();
-      $('.wrapper__chat-main__message-list').animate({ scrollTop: $('.wrapper__chat-main__message-list')[0].scrollHeight});
-      $('.submit').prop('disabled', false);
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
-  });
+      .done(function (messages) {
+        if (messages.length !== 0) {
+          let insertHTML = '';
+          $.each(messages, function (i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.wrapper__chat-main__message-list').append(insertHTML);
+          $('.wrapper__chat-main__message-list').animate({ scrollTop: $('.wrapper__chat-main__message-list')[0].scrollHeight });
+        }
+      })
+      .fail(function () {
+        alert('error');
+      });
+  };
+  setInterval(reloadMessages, 15000);
 });
